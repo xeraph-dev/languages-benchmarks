9aad97095a95bb59063c6b69d7d492e3df4c4a51
Ross <79549577+ross96D@users.noreply.github.com>
2023-12-24 18:41:23 -0500

    [go] Advent of Code - Year 2015 - Day 4 - part 2 (#3)
    * add golang solution
    
    * add: solution for part 2
    
    * add ultra fast paralelism
    
    * move part2 to root
    
    * [go] split by challenges
    
    ---------
    
    Co-authored-by: Xeraph <61700204+xeraph-dev@users.noreply.github.com>

---
+++ go/cmd/aoc-year2015-day4/part1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+)
+
+const compare uint32 = 4095
+
+var inputHash = "yzbqklnj"
+
+func main() {
+	for i := 0; ; i++ {
+		h := md5.New()
+		in := inputHash + strconv.Itoa(i)
+		h.Write([]byte(in))
+		b := h.Sum(nil)
+		x := binary.BigEndian.Uint32(b[0:4])
+		if x|compare == compare {
+			fmt.Printf("%d", i)
+			break
+		}
+	}
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/xeraph-dev/languages-benchmarks
+
+go 1.21.0
