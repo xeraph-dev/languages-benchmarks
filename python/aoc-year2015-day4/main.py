d881eb103b9aaaf2f1d5a1767154f1dd820b76ec
nyan <nyan@nomail.org>
2023-12-25 09:14:27 -0500

    OwO

---
+++ python/aoc-year2015-day4/main.py
@@ -0,0 +1,64 @@
+## WARNING: Python 3.12 only
+from threading import Thread
+import _xxsubinterpreters as interpreters
+import _xxinterpchannels as channels
+
+
+WORKERS = [interpreters.create() for x in range(8)]
+CHUNK_SIZE = 10000
+
+channel = channels.create()
+
+worker_payload = """
+# Needs (channel: int, prefix: str, zeroes: int, start: int, end: int):
+from hashlib import md5
+import _xxinterpchannels as channels
+compare_zeroes: str = "0" * zeroes
+prehash = md5(prefix.encode())
+for i in range(start, end):
+    hash = prehash.copy()
+    hash.update(str(i).encode())
+    if hash.hexdigest().startswith(compare_zeroes):
+        channels.send(channel, i)
+        break
+channels.send(channel, None)
+"""
+
+
+def compute(prefix: str, zeroes: int):
+    start = 0
+    solutions = []
+
+    while True:
+        for worker in WORKERS:
+            while interpreters.is_running(worker):
+                pass
+            Thread(
+                target=interpreters.run_string,
+                kwargs={
+                    "script": worker_payload,
+                    "id": worker,
+                    "shared": {
+                        "channel": channel,
+                        "prefix": prefix,
+                        "zeroes": zeroes,
+                        "start": start,
+                        "end": start + CHUNK_SIZE,
+                    },
+                },
+            ).start()
+            start += CHUNK_SIZE
+
+        for i in range(len(WORKERS) + 1):
+            try:
+                possible_solution = channels.recv(channel)
+                if possible_solution != None:
+                    solutions.append(possible_solution)
+            except:
+                if solutions:
+                    return min(solutions)
+                else:
+                    break
+
+
+print("Result:", compute(prefix="yzbqklnj", zeroes=6))
