30f6e19eb9e3024e03c166798e66876897f66c30
nyan <nyan@nomail.org>
2023-12-28 00:15:53 -0500

    Better, faster, stronger

--- go/nyan/main.go
+++ go/nyan/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/md5"
-	"encoding/binary"
 	"fmt"
 	"os"
 	"runtime"
@@ -10,35 +9,35 @@ import (
 	"sync"
 )
 
+func getStartingZeroesCount(slice []byte) (zeroes int) {
+	for _, value := range slice {
+		if value == 0x00 {
+			zeroes += 2
+			continue
+		}
+		if value <= 0x0F {
+			zeroes++
+		}
+		break
+	}
+	return zeroes
+}
+
 func compute(prefix string, zeroes int) int {
-	candidateSolution := -1
 	var waitGroup sync.WaitGroup
 	workerCount := runtime.NumCPU()
 
-	var compare1 uint64 = 0xFFFFFFFFFFFFFFFF
-	var compare2 uint64 = 0xFFFFFFFFFFFFFFFF
-
-	if zeroes < 16 {
-		compare1 = compare1 >> (zeroes * 4)
-	} else if zeroes < 32 {
-		compare1 = 0
-		compare2 = compare2 >> ((zeroes - 16) * 4)
-	} else {
-		compare1 = 0
-		compare2 = 0
-	}
-
+	bytePrefix := []byte(prefix)
+	candidateSolution := -1
 	worker := func(start int, step int) {
 		defer waitGroup.Done()
-
+		hash := md5.New()
 		for i := start; candidateSolution == -1 || i < candidateSolution; i += step {
-			hash := md5.New()
-			hash.Write([]byte(prefix + strconv.Itoa(i)))
+			hash.Write(bytePrefix)
+			hash.Write([]byte(strconv.Itoa(i)))
 			hashSum := hash.Sum(nil)
-			hashNum1 := binary.BigEndian.Uint64(hashSum[0:8])
-			hashNum2 := binary.BigEndian.Uint64(hashSum[8:16])
-			if hashNum1 <= compare1 && hashNum2 <= compare2 {
-				fmt.Printf("Zeroes: %v, Found: %v - %x\n", zeroes, prefix+strconv.Itoa(i), hashSum)
+			hash.Reset()
+			if getStartingZeroesCount(hashSum) >= zeroes {
 				candidateSolution = i
 				return
 			}
@@ -55,25 +54,17 @@ func compute(prefix string, zeroes int) int {
 }
 
 func main() {
-	var prefix string
-	var zeroes int
-
 	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %v prefix zeroes\n"+
-			"No parameters specified, will use 'yzbqklnj 6'\n",
-			os.Args[0])
+		fmt.Printf("Usage: %v prefix zeroes\n", os.Args[0])
+		os.Exit(1)
 
-		prefix = "yzbqklnj"
-		zeroes = 6
-	} else {
-		prefix = os.Args[1]
+	}
 
-		var err error
-		zeroes, err = strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Printf("ERROR: '%v' is not an integer\n", os.Args[2])
-			os.Exit(1)
-		}
+	prefix := os.Args[1]
+	zeroes, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("ERROR: '%v' is not an integer\n", os.Args[2])
+		os.Exit(1)
 	}
 
 	if zeroes > 32 || zeroes < 0 {
@@ -82,6 +73,5 @@ func main() {
 	}
 
 	solution := compute(prefix, zeroes)
-	fmt.Printf("Solution: %v\n", solution)
-
+	fmt.Printf("%v\n", solution)
 }
