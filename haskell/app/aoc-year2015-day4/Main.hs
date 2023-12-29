fcedcf0b5b4ff180e955a7c0be80086b307d04b3
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2023-12-29 11:29:50 -0500

    [haskell] performance improvement

--- haskell/app/aoc-year2015-day4/Main.hs
+++ haskell/app/aoc-year2015-day4/Main.hs
@@ -1,16 +1,28 @@
 module Main (main) where
 
-import Crypto.Hash (Digest, MD5, hashlazy)
-import qualified Data.ByteString.Lazy.Char8 as BL
+import Crypto.Hash (Context, MD5 (MD5), hashFinalize, hashInitWith, hashUpdate)
+import qualified Data.ByteArray as BA
+import qualified Data.ByteString.Char8 as BL
 import Data.List (find)
 import System.Environment (getArgs)
+
+compute :: Context MD5 -> Int -> BA.Bytes -> Int -> Bool
+compute hash bytes_count zeros i =
+  let num = BL.pack $ show (i :: Int)
+      digest = hashFinalize . hashUpdate hash $ num
+      bytes = BA.take bytes_count $ BA.convert digest :: BA.Bytes
+   in bytes <= zeros
 
 main :: IO ()
 main = do
   args <- getArgs
 
   let secret = BL.pack $ head args
-  let zeros_count = read $ args !! 1 :: Int
-  case find (\i -> take zeros_count (show (hashlazy (secret <> BL.pack (show (i :: Integer))) :: Digest MD5)) == replicate zeros_count '0') [1 ..] of
+      zeros_count = read $ args !! 1 :: Int
+      bytes_count = ceiling $ fromIntegral zeros_count / 2
+      zeros = BA.snoc (BA.replicate (div zeros_count 2) 0 :: BA.Bytes) 0xF
+      hash = hashUpdate (hashInitWith MD5) secret
+
+  case find (compute hash bytes_count zeros) [1 ..] of
     Just x -> print x
     Nothing -> putStrLn "wtf"
