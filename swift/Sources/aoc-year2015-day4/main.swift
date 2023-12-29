cf8461ed6a26e92592868bef5f644ad64d95871f
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2023-12-28 19:01:15 -0500

    [swift] small performance improvement

--- swift/Sources/aoc-year2015-day4/main.swift
+++ swift/Sources/aoc-year2015-day4/main.swift
@@ -1,15 +1,34 @@
 import CryptoKit
 import Foundation
 
-let zeros = [UInt8](repeating: 0, count: 3)[...]
+extension Digest {
+    var startingZeros: UInt8 {
+        var zeros: UInt8 = 0
+        for byte in self {
+            if byte == 0x00 {
+                zeros += 2
+                continue
+            }
+            if byte <= 0x0F {
+                zeros += 1
+            }
+            break
+        }
+        return zeros
+    }
+}
+
+var args = CommandLine.arguments
+var secret = args[1]
+var zeros = UInt8(args[2])!
 
 var hash = Insecure.MD5()
-hash.update(data: [UInt8]("yzbqklnj".utf8))
+hash.update(data: [UInt8](secret.utf8))
 
 for i in 0 ... UInt32.max {
     var hash = hash
     hash.update(data: [UInt8](i.description.utf8))
-    if [UInt8](hash.finalize())[0 ... 2] == zeros {
+    if hash.finalize().startingZeros == zeros {
         print(i)
         break
     }
