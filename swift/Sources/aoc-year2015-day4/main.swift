9e1519ff5a0c857d61ef4132178f75f17d7f87e4
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2023-12-30 13:59:22 -0500

    [swift] implement multi threading

--- swift/Sources/aoc-year2015-day4/main.swift
+++ swift/Sources/aoc-year2015-day4/main.swift
@@ -25,11 +25,30 @@ var zeros = UInt8(args[2])!
 var hash = Insecure.MD5()
 hash.update(data: [UInt8](secret.utf8))
 
-for i in 0 ... UInt32.max {
-    var hash = hash
-    hash.update(data: [UInt8](i.description.utf8))
-    if hash.finalize().startingZeros == zeros {
-        print(i)
-        break
+let processorCount = ProcessInfo().processorCount
+
+let group = DispatchGroup()
+
+var candidate: UInt32?
+
+for steps in 0 ..< UInt32(processorCount) {
+    Task {
+        group.enter()
+        for i in stride(from: steps, to: UInt32.max, by: processorCount) {
+            guard candidate == nil || i < candidate! else {
+                group.leave()
+                return
+            }
+
+            var hash = hash
+            hash.update(data: [UInt8](i.description.utf8))
+            if hash.finalize().startingZeros == zeros {
+                candidate = i
+            }
+        }
     }
 }
+
+group.wait()
+
+print(candidate!)
