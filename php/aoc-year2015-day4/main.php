80df100b79b7c0e57fbbfb1fdd39d953e2269ef1
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-01-01 07:44:43 -0500

    [php] accept arguments

--- php/aoc-year2015-day4/main.php
+++ php/aoc-year2015-day4/main.php
@@ -1,18 +1,23 @@
 <?php
 
-function mineAdventCoin($secretKey)
-{
-    $number = 0;
-    $target = "\x00\x00\x00";
+$secret = $argv[1];
+$zeros = $argv[2];
 
-    while (true) {
-        $hash = md5($secretKey . $number, true);
-        if (strncmp($hash, $target, 3) === 0) {
-            return ['number' => $number, 'hash' => bin2hex($hash)];
-        }
-        $number++;
+
+$half = $zeros / 2;
+$bytes_count = ceil($half);
+$number = 0;
+$target = str_repeat("\x00", $bytes_count - 1);
+$target .= $half == $bytes_count ? "\x00" : "\x0F";
+
+$hash = md5($secret . 282749, true);
+
+while (true) {
+    $hash = md5($secret . $number, true);
+    if (strncmp($hash, $target, $bytes_count) <= 0) {
+        break;
     }
+    $number++;
 }
 
-$secretKey = 'yzbqklnj';
-var_dump(mineAdventCoin($secretKey));
+echo $number;
